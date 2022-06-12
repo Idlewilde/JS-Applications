da9cc99c6c34b0043c3865e29abd9a4c69989ca5
Tonya Kostova <91955165+Idlewilde@users.noreply.github.com>
2022-06-12 20:34:08 +0300

    Add files via upload

--- Asynchronous programming/Lab/03.Gighub-Commits/app.js
+++ Asynchronous programming/Lab/03.Gighub-Commits/app.js
@@ -1,4 +1,38 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+   let username = document.getElementById(`username`);
+	let	repo=document.getElementById(`repo`);
+	let	commits = document.getElementById(`commits`);
+    let url=`https://api.github.com/repos/${username.value}/${repo.value}/commits`
+
+    fetch(url)
+    .then(res => {
+        if (res.ok == false) {
+            throw new Error(`Error: <${res.status}> (Not Found)`)
+        }
+        return res.json()
+    })
+    .then(handleResponse)
+    .catch(handleError);
+
+function handleResponse(data) {
+    commits.innerHTML = '';
+    console.log(data);
+    for (let commit of data) {
+        let liElement = document.createElement('li');
+        let obj = commit.commit;
+        let name =obj.author.name
+        console.log(obj.message);
+        liElement.innerHTML = `${name}: ${obj.message}`
+        commits.appendChild(liElement);
+    }
+}
+
+function handleError(error) {
+    commits.innerHTML = '';
+    "Error: <error.status> (Not Found)"
+    let liElement = document.createElement('li');
+        liElement.innerHTML = `${error.message}`
+        commits.appendChild(liElement);
+}
+        
 }
